acdeff29aa48b9346c242ffd6f6132eb335c5967
rion suzuki <rion2002m@gmail.com>
2023-07-07 15:11:16 +0900

    ファイル名xが×（掛ける）になっていたものを修正

--- application/LoadManager.cpp
+++ application/LoadManager.cpp
@@ -13,7 +13,7 @@ void LoadManager::LoadModel()
 
 void LoadManager::LoadTexture()
 {
-	LoadGraph("white1Å~1.png", "White");
+	LoadGraph("white1x1.png", "White");
 	LoadGraph("white1280x720.png", "White1280x720");
 	LoadGraph("test.png", "Test");
 	LoadGraph("uv.png", "uv");
