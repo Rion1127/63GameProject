e869a7c146565bd6c1fd07a16503a84d271bfe9d
rion suzuki <rion2002m@gmail.com>
2023-08-17 02:50:49 +0900

    魔法用のファイル追加

--- application/Sourcecode/Player/MagicManager.cpp
+++ application/Sourcecode/Player/MagicManager.cpp
@@ -1,1 +1,13 @@
 #include "MagicManager.h"
+
+MagicManager::MagicManager()
+{
+}
+
+void MagicManager::Update()
+{
+}
+
+void MagicManager::Draw()
+{
+}
--- application/Sourcecode/Player/MagicManager.h
+++ application/Sourcecode/Player/MagicManager.h
@@ -1,9 +1,16 @@
 #pragma once
+#include "IAttack.h"
+#include "IBullet.h"
 class MagicManager
 {
 private:
+	std::vector<IAttack> magics_;
+	std::vector<IAttack> magics_;
 
 public:
+	MagicManager();
 	void Update();
+
+	void Draw();
 };
 
