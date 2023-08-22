248f52eff4436607f5eda2800aec84298b2d132c
rion suzuki <rion2002m@gmail.com>
2023-08-23 00:05:34 +0900

    操作UI変更

--- application/Sourcecode/UI/UIOperation.cpp
+++ application/Sourcecode/UI/UIOperation.cpp
@@ -9,18 +9,18 @@ UIOperation::UIOperation()
 		operationUI_[i] = MakeUnique<Sprite>();
 		operationUI_[i]->Ini();
 		operationUI_[i]->SetTexture(TextureManager::GetInstance()->GetTexture("OperationUI"));
-		operationUI_[i]->SetTex_Size({ 128,64 });
+		operationUI_[i]->SetTex_Size({ 128,128 });
 
-		Vector2 pos = {225.f + i * 200.f,670};
+		Vector2 pos = {225.f + i * 200.f,690};
 		operationUI_[i]->SetPos(pos);
-		operationUI_[i]->SetScale(Vector2(0.3f, 0.3f));
+		operationUI_[i]->SetScale(Vector2(0.3f, 0.6f));
 
 		textUI_[i] = MakeUnique<Sprite>();
 		textUI_[i]->Ini();
 		textUI_[i]->SetTexture(TextureManager::GetInstance()->GetTexture("UItex"));
 		textUI_[i]->SetTex_Size({ 128,64 });
 
-		pos = { operationUI_[i]->GetPos().x + 100.f,675.f};
+		pos = { operationUI_[i]->GetPos().x + 100.f,650.f};
 		textUI_[i]->SetPos(pos);
 		textUI_[i]->SetScale(Vector2(0.4f,0.4f));
 	}
@@ -31,7 +31,7 @@ UIOperation::UIOperation()
 	operationUI_[1]->SetTex_LeftTop({ 256,32 });
 	textUI_[1]->SetTex_LeftTop({ 128,0 });
 
-	operationUI_[2]->SetTex_LeftTop({ 384,160 });
+	operationUI_[2]->SetTex_LeftTop({ 384,155 });
 	textUI_[2]->SetTex_LeftTop({ 0,64 });
 
 	operationUI_[3]->SetTex_LeftTop({ 384,32 });
