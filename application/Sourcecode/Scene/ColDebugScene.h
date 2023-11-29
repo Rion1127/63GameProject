6a2138db9df93c7aa19faaf27e8ba74f85794a21
rion suzuki <rion2002m@gmail.com>
2023-11-29 15:09:47 +0900

    上下を制限した球の当たり判定実装

--- application/Sourcecode/Scene/ColDebugScene.h
+++ application/Sourcecode/Scene/ColDebugScene.h
@@ -1,16 +1,23 @@
 #pragma once
 #include "IScene.h"
 #include "Object3d.h"
+#include "Collision.h"
+#include "LightManager.h"
+#include "DebugCamera.h"
 
 class ColDebugScene
 	final :
 	public IScene
 {
 private:
-	std::unique_ptr<Object3d> col1_;
-	std::unique_ptr<Object3d> col2_;
+	std::shared_ptr<LightManager> lightManager_ = nullptr;
+	std::unique_ptr<Object3d> col1Obj_;
+	std::unique_ptr<Object3d> col2Obj_;
 
+	Sphere col1_;
+	Sphere col2_;
 
+	DebugCamera debugCamera_;
 public:
 	~ColDebugScene();
 
